b47ff576c4a48ff24d2675c5dbd7abcb6f214180
Messaoud-SETTOU <messoudsettou@gmail.com>
2023-03-08 08:58:17 +0100

    "update radio function "

--- radio/radio.js
+++ radio/radio.js
@@ -1,73 +1,7 @@
-// a function that targets the element of the audio recources in diffrent tags and plays it with the .play() methode
-function playr1(){     
-    document.getElementById("rr1").play()
-} 
-function playr2(){
-    document.getElementById("rr2").play()
-}
-  
-function playr3(){
-    document.getElementById("rr3").play()
-}
-function playr4(){
-    document.getElementById("rr4").play()
-}
-function playr5(){
-    document.getElementById("rr5").play()
-}
+function play(audio) {     
+    document.getElementById(audio).play()  // function that takes a source audio as a parameter and play it 
+  }
 
-function playr6(){
-    document.getElementById("rr6").play()
-}
-function playr7(){
-    document.getElementById("rr7").play()
-}
-function playr8(){
-    document.getElementById("rr8").play()
-}
-function playr9(){
-    document.getElementById("rr9").play()
-}
-function playr10(){
-    document.getElementById("rr10").play()
-}
-// a function that targets the element of the audio recources in diffrent tags and pause it with the .pause() methode
-function pause1(){
-    document.getElementById("rr1").pause()
-} 
-function pause2(){
-    document.getElementById("rr2").pause()
-} 
-
-function pause3(){
-    document.getElementById("rr3").pause()
-} 
-
-function pause4(){
-    document.getElementById("rr4").pause()
-} 
-
-function pause5(){
-    document.getElementById("rr5").pause()
-} 
-
-function pause6(){
-    document.getElementById("rr6").pause()
-} 
-
-function pause7(){
-    document.getElementById("rr7").pause()
-} 
-
-function pause8(){
-    document.getElementById("rr8").pause()
-} 
-function pause9(){
-    document.getElementById("rr9").pause()
-} 
-
-function pause10(){
-    document.getElementById("rr10").pause()
-} 
-
-
+  function pause(audio) {
+    document.getElementById(audio).pause() // function that takes a source audio as a parameter and pause it 
+  }
